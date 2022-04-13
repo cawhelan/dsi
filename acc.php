651beff5a93a5d7921087e3dfcb84064f9fe9159
tsundoko <eurxpe@gmail.com>
2022-04-12 21:42:59 -0400

    Implementation of php files

---
+++ acc.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Store Data</title>
+</head>
+
+<body>
+	<center>
+	<form action="insert.php">
+        <label for="username">Username: </label>
+        <input type="text" id="username" name="username">
+        <br><label for="password">Password: </label>
+        <input type="text" id="password" name="password">
+        <input type="submit" value="Submit">
+			
+		<input type="reset" value="Reset"/>
+    </form>
+	</center>
+</body>
+</html>
---
+++ backend.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Store Data</title>
+</head>
+
+<body>
+	<center>
+	<form action="insert.php" method="post">
+		<label for="username">Username: </label>
+        <input type="text" id="username" name="username">
+        <br><label for="password">Password: </label>
+        <input type="text" id="password" name="password">
+        <br><button type="button" class="btn btn-danger btn-sm">Submit</button>	
+	</form>
+	</center>
+</body>
+</html>
